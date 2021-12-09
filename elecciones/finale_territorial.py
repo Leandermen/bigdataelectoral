76a9e2eaa02b141b6e13396f57544065b70facd3
Leandro Zamudio <leandro.zamudioleon@gmail.com>
2021-12-09 18:25:48 -0300

    finale_territorial beta1

--- elecciones/finale_territorial.py
+++ elecciones/finale_territorial.py
@@ -8,26 +8,49 @@ from arcgis.gis import GIS
 from arcgis.features import GeoAccessor, GeoSeriesAccessor
 from datetime import datetime
 
-#Global
-def GlobalNational(tercore):
+gis = GIS("https://www.arcgis.com", 'soportaltda', 'Mhilo.2016')
+ItemSource=gis.content.get('d82682aef8f440deb1a35129502ae5a7')
+#ItemTS=gis.content.get('39107a24dd2847b2b3c41f1fe594c354')
+paisext=ItemSource.layers[2]
+comuchl=ItemSource.layers[3]
+provchl=ItemSource.layers[4]
+regichl=ItemSource.layers[5]
+layergroup={'pais':paisext,'regiones':regichl,'provincias':provchl,'comunas':comuchl}
+
+def obtenerservicio(ambito):
+    return layergroup[ambito]
+
+def clasificador(json,territorio):
+    inarray=[]
+    for record in json:
+        if record['amb']==territorio:        
+            inoid=record['oid']
+            inids=record['ids']
+            ter=territorio
+            subarray=[inoid,inids,ter]
+            inarray.append(subarray)
+        else: pass
+    return inarray
+
+def Territorial(tercore):
     fcoid=tercore[0]
     idservel=tercore[1]
     ambito=tercore[2]
     #Timestamps
     dt=datetime.now()
     #Obtención de Jsons
-    resultados="https://www.servelelecciones.cl/data/elecciones_presidente/computo/global/19001.json"
-    mesas="https://www.servelelecciones.cl/data/mesas_instaladas/computo/global/19001.json"
-    padron="https://www.servelelecciones.cl/data/participacion/computo/global/19001.json"
+    resultados="https://www.servelelecciones.cl/data/elecciones_presidente/computo/{}/{}.json".format(ambito,idservel)
+    mesas="https://www.servelelecciones.cl/data/mesas_instaladas/computo/{}/{}.json".format(ambito,idservel)
+    padron="https://www.servelelecciones.cl/data/participacion/computo/{}/{}.json".format(ambito,idservel)
     rresults = requests.request("GET", resultados, headers={}, data={})
     jresults=json.loads(rresults.text)
     rmesas = requests.request("GET", mesas, headers={}, data={})
     jmesas=json.loads(rmesas.text)
     rpadron = requests.request("GET", padron, headers={}, data={})
     jpadron=json.loads(rpadron.text)
-    #mainnational=ItemSource.tables[0]
-    qnation=mainnational.query(out_fields='*')
-    modregister=[f for f in qnation][0]
+    servicio=obtenerservicio(ambito)
+    qnation=servicio.query(out_fields='*',return_geometry=False)
+    modregister=[f for f in qnation if f.attributes['OBJECTID']==fcoid][0]
     modregister.attributes['ts']=dt
     #Resultados 1V
     modregister.attributes['c11v']=int(jresults['data'][0]['c'].replace(".",""))
@@ -62,19 +85,15 @@ def GlobalNational(tercore):
     if modregister.attributes['tPad'] != 0:
         modregister.attributes['cv1v']=round((modregister.attributes['vt1v']/modregister.attributes['tPad'])*100,3)
     else: modregister.attributes['cv1v']=0
-    print("Preparado")
-    return modregister
+    print("Preparado "+str(idservel))
+    servicio.edit_features(updates=[modregister])
     
     
 if __name__ == '__main__':
-    #UTerritorial=['regiones','provincias','comunas','pais']
-    print('Computo Global en Progreso')
-    gis = GIS("https://www.arcgis.com", 'soportaltda', 'Mhilo.2016')
-    ItemSource=gis.content.get('d82682aef8f440deb1a35129502ae5a7')
-    ItemTS=gis.content.get('39107a24dd2847b2b3c41f1fe594c354')
-    ttsnation=ItemTS.tables[0]
-    tnation=ItemSource.tables[0]
-    arreglo=GlobalNational(tnation)
-    tnation.edit_features(updates=[arreglo])
-    ttsnation.edit_features(adds=[arreglo])
+    print('Inicio de Edición')
+    keyindex=open('elecciones/inputs/codigosfs.json')
+    jkey=json.load(keyindex)
+    testinput=clasificador(jkey,'regiones')
+    with Pool(3) as p:
+        p.map(Territorial,testinput)
     print("listo")
