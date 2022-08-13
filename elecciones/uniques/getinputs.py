585df477c6c88e1dd4cb03ceb1f51996d45cc4fa
Leandro Zamudio <leandro.zamudioleon@gmail.com>
2022-08-13 02:55:54 -0400

    BDE-7 Conexión con inputs nuevos

--- elecciones/uniques/getinputs.py
+++ elecciones/uniques/getinputs.py
@@ -1,12 +1,15 @@
 import requests
 import json
 
-continentlist="https://www.servelelecciones.cl/data/mesas_instaladas/filters/continentes/all.json"
-countrylist="https://www.servelelecciones.cl/data/mesas_instaladas/filters/paises/all.json"
-reglist="https://www.servelelecciones.cl/data/elecciones_presidente/filters/regiones/all.json"
-provlist="https://www.servelelecciones.cl/data/elecciones_presidente/filters/provincias/all.json"
-comlist="https://www.servelelecciones.cl/data/elecciones_presidente/filters/comunas/all.json"
-circelectoral="https://www.servelelecciones.cl/data/elecciones_presidente/filters/circ_electoral/allchile.json"
+context_event='elecciones_constitucion'
+folder='salida2022'
+
+continentlist="https://www.servelelecciones.cl/data/{}/filters/continentes/all.json".format(context_event)
+countrylist="https://www.servelelecciones.cl/data/{}/filters/paises/all.json".format(context_event)
+reglist="https://www.servelelecciones.cl/data/{}/filters/regiones/all.json".format(context_event)
+provlist="https://www.servelelecciones.cl/data/{}/filters/provincias/all.json".format(context_event)
+comlist="https://www.servelelecciones.cl/data/{}/filters/comunas/all.json".format(context_event)
+circelectoral="https://www.servelelecciones.cl/data/{}/filters/circ_electoral/allchile.json".format(context_event)
 
 rcon = requests.request("GET", continentlist, headers={}, data={})
 rpai = requests.request("GET", countrylist, headers={}, data={})
@@ -22,20 +25,20 @@ jpro=json.loads(rpro.text)
 jcom=json.loads(rcom.text)
 jcce=json.loads(rcce.text)
 
-with open('elecciones/inputs/continentes.json', 'w', encoding='utf-8') as f:
+with open('elecciones/{}/continentes.json'.format(folder), 'w', encoding='utf-8') as f:
     json.dump(jcon, f,ensure_ascii=False, indent=4)
 
-with open('elecciones/inputs/pais.json', 'w', encoding='utf-8') as f:
+with open('elecciones/{}/pais.json'.format(folder), 'w', encoding='utf-8') as f:
     json.dump(jpai, f,ensure_ascii=False, indent=4)
 
-with open('elecciones/inputs/regiones.json', 'w', encoding='utf-8') as f:
+with open('elecciones/{}/regiones.json'.format(folder), 'w', encoding='utf-8') as f:
     json.dump(jreg, f,ensure_ascii=False, indent=4)
 
-with open('elecciones/inputs/provincias.json', 'w', encoding='utf-8') as f:
+with open('elecciones/{}/provincias.json'.format(folder), 'w', encoding='utf-8') as f:
     json.dump(jpro, f,ensure_ascii=False, indent=4)
 
-with open('elecciones/inputs/comunas.json', 'w', encoding='utf-8') as f:
+with open('elecciones/{}/comunas.json'.format(folder), 'w', encoding='utf-8') as f:
     json.dump(jcom, f,ensure_ascii=False, indent=4)
 
-with open('elecciones/inputs/circelectoral.json', 'w', encoding='utf-8') as f:
+with open('elecciones/{}/circelectoral.json'.format(folder), 'w', encoding='utf-8') as f:
     json.dump(jcce, f,ensure_ascii=False, indent=4)
