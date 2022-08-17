fb91f0b1720a90366140edc311e9eebed3a1a5d4
Leandro Zamudio <leandro.zamudioleon@gmail.com>
2022-08-17 00:19:51 -0400

    BDE-11 BDE-18 timetracerf1 computo nacional

--- pleb2022/exec/timetracerf1.py
+++ pleb2022/exec/timetracerf1.py
@@ -8,23 +8,29 @@ from arcgis.gis import GIS
 from arcgis.features import GeoAccessor, GeoSeriesAccessor
 from datetime import datetime
 
-masterurl="https://www.servelelecciones.cl/data/mesas_instaladas/computo/global/19001.json"
+event_context='mesas_constituidas'
+evento='pleb2022'
+folder='lookups'
+
+masterurl="https://www.servelelecciones.cl/data/{}/computo/global/19001.json".format(event_context)
 dato=1
 update=0
 
 gis = GIS("https://www.arcgis.com", 'soportaltda', 'Mhilo.2016')
-ItemSource=gis.content.get('d82682aef8f440deb1a35129502ae5a7')
-ItemTS=gis.content.get('39107a24dd2847b2b3c41f1fe594c354')
+#ItemSource=gis.content.get('d82682aef8f440deb1a35129502ae5a7')
+#ItemTS=gis.content.get('39107a24dd2847b2b3c41f1fe594c354')
+ItemSource=gis.content.get('dacbfde953734f3f9a3c9e604b89dfb1')
+ItemTS=gis.content.get('1cde3f8c703c457c9d7f91f8af42a004')
 ttsnation=ItemTS.tables[0]
 ttspais=ItemTS.tables[1]
-ttsregi=ItemTS.tables[5]
-ttsprov=ItemTS.tables[4]
-ttscomu=ItemTS.tables[2]
+ttsregi=ItemTS.tables[2]
+ttsprov=ItemTS.tables[3]
+ttscomu=ItemTS.tables[4]
 tnation=ItemSource.tables[0]
-paisext=ItemSource.layers[2]
-comuchl=ItemSource.layers[3]
-provchl=ItemSource.layers[4]
-regichl=ItemSource.layers[5]
+paisext=ItemSource.layers[5]
+comuchl=ItemSource.layers[4]
+provchl=ItemSource.layers[3]
+regichl=ItemSource.layers[2]
 layergroup={'pais':paisext,'regiones':regichl,'provincias':provchl,'comunas':comuchl}
 ttsgroup={'pais':ttspais,'regiones':ttsregi,'provincias':ttsprov,'comunas':ttscomu}
 
@@ -48,23 +54,19 @@ def clasificador(json):
 #Global
 def GlobalNational(mainnational):
     #Timestamps
-    print('Actualizando Mesas a Nivel Nacional')
+    print('Actualizando Mesas Constituidas a Nivel Nacional')
     dt=datetime.now()
     #Obtención de Jsons
-    mesas="https://www.servelelecciones.cl/data/mesas_instaladas/computo/global/19001.json"
+    mesas="https://www.servelelecciones.cl/data/{}/computo/global/19001.json".format(event_context)
     rmesas = requests.request("GET", mesas, headers={}, data={})
     jmesas=json.loads(rmesas.text)
     qnation=mainnational.query(out_fields='*')
     modregister=[f for f in qnation][0]
     modregister.attributes['ts']=dt
     #Mesas    
-    modregister.attributes['tM2v']=int(jmesas['resumen'][0]['b'].replace(".",""))
-    modregister.attributes['iM2v']=int(jmesas['resumen'][0]['c'].replace(".",""))
-    modregister.attributes['pM2v']=int(jmesas['resumen'][0]['d'].replace(".",""))
-    if modregister.attributes['tM2v'] != 0:
-        modregister.attributes['cM2v']=round((modregister.attributes['iM2v']/modregister.attributes['tM2v'])*100,3)
-    else: modregister.attributes['cM2v']=0
-    print("Preparado")
+    modregister.attributes['cM']=int(jmesas['resumen'][0]['c'].replace(".",""))
+    modregister.attributes['dcM']=modregister.attributes['mesas']-int(jmesas['resumen'][0]['c'].replace(".",""))
+    print("Preparado Computo Global")
     tnation.edit_features(updates=[modregister])
     ttsnation.edit_features(adds=[modregister])
 
@@ -75,7 +77,7 @@ def Territorial(tercore):
     #Timestamps
     dt=datetime.now()
     #Obtención de Jsons
-    mesas="https://www.servelelecciones.cl/data/mesas_instaladas/computo/{}/{}.json".format(ambito,idservel)
+    mesas="https://www.servelelecciones.cl/data/{}/computo/{}/{}.json".format(event_context,ambito,idservel)
     rmesas = requests.request("GET", mesas, headers={}, data={})
     jmesas=json.loads(rmesas.text)
     servicio=obtenerservicio(ambito,0)
@@ -100,7 +102,7 @@ def CheckNovedad(url):
     return mesasinst
 
 if __name__ == '__main__':
-    keyindex=open('elecciones/inputs/codigosfs.json')
+    keyindex=open('{}/{}/codigosfs.json'.format(evento,folder))
     jkey=json.load(keyindex)
     terinput=clasificador(jkey)
     while True:
@@ -108,8 +110,8 @@ if __name__ == '__main__':
         if update!=dato:
             stime=datetime.now()
             GlobalNational(tnation)
-            with Pool(4) as p:
-                p.map(Territorial,terinput)
+            #with Pool(4) as p:
+            #    p.map(Territorial,terinput)
             dato=update
             etime=datetime.now()
             timedelta=etime-stime
