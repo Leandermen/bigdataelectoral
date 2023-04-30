91aa70e45880f8604a1fcf74b5fb10748c8a0d08
Leandro Zamudio <leandro.zamudioleon@gmail.com>
2023-04-30 12:57:11 -0400

    unificada data lista locales

--- cc2023/refs/02listalocales_once.py
+++ cc2023/refs/02listalocales_once.py
@@ -15,7 +15,11 @@ folder='lookups'
 s = requests.Session()
 ce=open('{}/{}/circelectoral.json'.format(evento,folder),encoding='utf-8')
 jce=json.load(ce)
-tabla=pd.DataFrame(columns=['circelec','nce','idservel','local','kei'])
+tabla=pd.DataFrame(columns=['c_circ','nce','idservel','local','kei'])
+
+reg=open("{}/{}/regiones.json".format(evento,folder))
+jreg=json.load(reg)
+tablapres=pd.DataFrame(columns=['c_reg','c_prov','c_com','c_circ'])
 
 for key in jce:
     k=key['c']
@@ -31,9 +35,42 @@ for key in jce:
         a=esc['c']
         b=esc['d']
         j=b+'0p0'+d
-        fila={'circelec':k,'nce':d,'idservel':a,'local':b,'kei':j}
+        fila={'c_circ':k,'nce':d,'idservel':a,'local':b,'kei':j}
         tabla=tabla.append(fila,ignore_index=True)
 
-tabla.to_json(path_or_buf='{}/{}/localesservel.json'.format(evento,folder),orient='records',force_ascii=False)
-tabla.to_csv(path_or_buf='{}/{}/localesservelt.csv'.format(evento,folder),index=False,encoding='utf-8')
+for region in jreg:
+    creg=region['c']
+    urlprov="https://www.servelelecciones.cl/data/{}/filters/provincias/byregion/{}.json".format(context_event,creg)
+    rpprov = requests.request("GET", urlprov, headers={}, data={})
+    provincias=json.loads(rpprov.text)
+    for provincia in provincias:
+        cpro=provincia['c']
+        urlcomuna="https://www.servelelecciones.cl/data/{}/filters/comunas/byprovincia/{}.json".format(context_event,cpro)
+        rpcom = requests.request("GET", urlcomuna, headers={}, data={})
+        comunas=json.loads(rpcom.text)
+        for comuna in comunas:
+            ccom=comuna['c']
+            ncom=comuna['d']
+            print(ncom)
+            urlcirc="https://www.servelelecciones.cl/data/{}/filters/circ_electoral/bycomuna/{}.json".format(context_event,ccom)
+            rpcirc = requests.request("GET", urlcirc, headers={}, data={})
+            circelecs=json.loads(rpcirc.text)
+            for circelec in circelecs:
+                ccel=circelec['c']
+                ncel=circelec['d']
+                fila={
+                    'c_reg':creg,
+                    'c_prov':cpro,
+                    'c_com':ccom,
+                    'c_circ':ccel
+                }
+                tablapres=tablapres.append(fila,ignore_index=True)
+
+shintabla = tabla.join(tablapres.set_index('c_circ'),on='c_circ')
+
+
+shintabla.to_csv(path_or_buf='{}/{}/tbllocalesservel.csv'.format(evento,folder),index=False,encoding='utf-8')
+#tabla.to_json(path_or_buf='{}/{}/localesservel.json'.format(evento,folder),orient='records',force_ascii=False)
+#tabla.to_csv(path_or_buf='{}/{}/tbllocalesservel.csv'.format(evento,folder),index=False,encoding='utf-8')
+reg.close()
 ce.close()
